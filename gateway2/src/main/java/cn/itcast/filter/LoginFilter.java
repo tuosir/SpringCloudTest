a5f0a1dcd249e5bacaa90692a0baa9de9be0faf8
tuosir <tuozhiyong123@qq.com>
2020-06-01 23:46:30 +0800

    zuul

---
+++ gateway2/src/main/java/cn/itcast/filter/LoginFilter.java
@@ -0,0 +1,47 @@
+package cn.itcast.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //获取请求上下文
+        RequestContext ctx=RequestContext.getCurrentContext();
+        //获取http请求
+        HttpServletRequest request=ctx.getRequest();
+        //获取请求参数
+        String token =  request.getParameter("access-token");
+        //判断是否存在
+        if(StringUtils.isBlank(token)){
+            ctx.setSendZuulResponse(false);
+            //返回403
+            ctx.setResponseStatusCode(HttpStatus.SC_FORBIDDEN);
+        }
+        //不存在，未登录，拦截
+        return null;
+    }
+}
